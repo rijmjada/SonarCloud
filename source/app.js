13df5907c4407d0c03808ccc04c2fede5823621a
Diego Ormeño <72719844+rijmjada@users.noreply.github.com>
2024-03-15 20:15:19 -0300

    Update app.js

--- source/app.js
+++ source/app.js
@@ -1,21 +1,10 @@
 const express = require('express');
 const app = express();
 
-
-// Error: Se está concatenando una cadena de consulta directamente en la consulta SQL, lo que puede llevar a inyección de SQL
-app.get('/usuarios', (req, res) => {
-    const userId = req.query.id;
-    const query = 'SELECT * FROM usuarios WHERE id=' + userId;
-    // Ejecutar la consulta SQL...
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo!');
 });
 
-// Puerto en el que se ejecutará el servidor
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor Express escuchando en el puerto ${PORT}`);
-});
-
-// Puerto en el que se ejecutará el servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor Express escuchando en el puerto ${PORT}`);
