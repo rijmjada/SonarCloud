750e2fcb9843fd09d95970b349058b4f953deeba
diego <rijmjada@gmail.com>
2024-03-15 19:36:36 -0300

    first commit

---
+++ source/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000; // Puedes cambiar el puerto si lo deseas
+
+// Ruta principal
+app2.get('/', (req, res) => {
+
+// Inicia el servidor
+app.listen(port, () => {
+  console.log(`La aplicación está corriendo en http://localhost:${port}`);
+});
